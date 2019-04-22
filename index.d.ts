564fca8101a2ee781e1575e9f45fcdc6dd280fcc
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-22 21:03:49 +0200

    Require Node.js 8, add TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,43 @@
+declare namespace splitAt {
+	interface Options {
+		/**
+		Remove the chosen indices.
+
+		Similar to the default `String#split` behaviour.
+
+		@default false
+		*/
+		readonly remove?: boolean;
+	}
+}
+
+/**
+Split a string at one or more indices.
+
+@param string - String to be split.
+@param index - One or more indices. A negative index is a 1-based position from the end of the string. For example, -1 is the index of the last place in the string. Duplicate indices are removed from the `index` array. A negative index and positive index that refer to the same position in the string are treated as duplicates.
+
+@example
+```
+import splitAt = require('split-at');
+
+splitAt('unicorn', 2);
+//=> ['uni', 'corn']
+
+splitAt('unicorn', -2);
+//=> ['unico', 'rn']
+
+splitAt('unicorn&rainbow', [6, 7]);
+//=> ['unicorn', '&', 'rainbow']
+
+splitAt('unicorn&rainbow', 7, {remove: true});
+//=> ['unicorn', 'rainbow']
+```
+*/
+declare function splitAt(
+	string: string,
+	index: number | readonly number[],
+	options?: splitAt.Options
+): string[];
+
+export = splitAt;
--- test.js
+++ test.js
@@ -1,47 +1,48 @@
-'use strict';
-var assert = require('assert');
-var test = require('ava');
-var splitAt = require('./');
+import test from 'ava';
+import splitAt from '.';
 
-test('splitAt()', function (t) {
-	assert.deepEqual(splitAt('unicorn', 2), ['uni', 'corn']);
-	assert.deepEqual(splitAt('unicorn', -2), ['unico', 'rn']);
+test('splitAt()', t => {
+	t.deepEqual(splitAt('unicorn', 2), ['uni', 'corn']);
+	t.deepEqual(splitAt('unicorn', -2), ['unico', 'rn']);
 
-	assert.deepEqual(
-		splitAt('unicorn & rainbows', [6, 13, 2, 9]),
-		['uni', 'corn', ' & ', 'rain', 'bows']
-	);
+	t.deepEqual(splitAt('unicorn & rainbows', [6, 13, 2, 9]), [
+		'uni',
+		'corn',
+		' & ',
+		'rain',
+		'bows'
+	]);
 
-	assert.deepEqual(
-		splitAt('unicorn & rainbows', [-6, -13, -2, -9]),
-		['unico', 'rn &', ' ra', 'inbo', 'ws']
-	);
+	t.deepEqual(splitAt('unicorn & rainbows', [-6, -13, -2, -9]), [
+		'unico',
+		'rn &',
+		' ra',
+		'inbo',
+		'ws'
+	]);
 
-	assert.deepEqual(splitAt('unicorn', 5), ['unicor', 'n']);
-	assert.deepEqual(splitAt('unicorn', 6), ['unicorn']);
-	assert.deepEqual(splitAt('unicorn', -1), ['unicor', 'n']);
-	assert.deepEqual(splitAt('unicorn', 100), ['unicorn']);
-	assert.deepEqual(splitAt('unicorn', -100), ['unicorn']);
-	assert.deepEqual(splitAt('unicorn', [-4, 2]), ['uni', 'corn']);
-
-	t.end();
+	t.deepEqual(splitAt('unicorn', 5), ['unicor', 'n']);
+	t.deepEqual(splitAt('unicorn', 6), ['unicorn']);
+	t.deepEqual(splitAt('unicorn', -1), ['unicor', 'n']);
+	t.deepEqual(splitAt('unicorn', 100), ['unicorn']);
+	t.deepEqual(splitAt('unicorn', -100), ['unicorn']);
+	t.deepEqual(splitAt('unicorn', [-4, 2]), ['uni', 'corn']);
 });
 
-test('remove option', function (t) {
-	assert.deepEqual(
-		splitAt('unicorn&rainbow', 7, {remove: true}),
-		['unicorn', 'rainbow']
-	);
+test('remove option', t => {
+	t.deepEqual(splitAt('unicorn&rainbow', 7, {remove: true}), [
+		'unicorn',
+		'rainbow'
+	]);
 
-	assert.deepEqual(
-		splitAt('unicorn&rainbow', -7, {remove: true}),
-		['unicorn', 'rainbow']
-	);
+	t.deepEqual(splitAt('unicorn&rainbow', -7, {remove: true}), [
+		'unicorn',
+		'rainbow'
+	]);
 
-	assert.deepEqual(
-		splitAt('foo|bar|baz', [3, 7], {remove: true}),
-		['foo', 'bar', 'baz']
-	);
-
-	t.end();
+	t.deepEqual(splitAt('foo|bar|baz', [3, 7], {remove: true}), [
+		'foo',
+		'bar',
+		'baz'
+	]);
 });
